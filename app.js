1f6d7a2581082153cee7eb4b7ad7d9d438976818
scheuerkev <scheuer.kevin@gmail.com>
2021-10-13 20:43:39 +0200

    Setting basic app

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const index = require('./routes');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(index);
+
+
+app.listen(port);
