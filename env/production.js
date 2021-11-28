27cb24892cfaefa8e89dac57aaa41e7b4807d5ee
scheuerkev <scheuer.kevin@gmail.com>
2021-11-28 13:14:32 +0100

    Setting app deployment

---
+++ env/production.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+    dbUrl: 'mongodb+srv://alex:qwe@cluster0.kqjyd.mongodb.net/twitter?retryWrites=true&w=majority',
+    cert: path.join(__dirname, ''),
+    key: path.join(__dirname, ''),
+    sessionSecret: 'zajilejk9804JKljskc90',
+}
